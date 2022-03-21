21676eb4e5055161d7e9f29d9d6a172cb5f8b010
leonardo.wolo <leonardo.wolo@aca-it.be>
2022-03-21 22:00:32 +0100

    lambda functie

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -7,7 +7,6 @@ from kubernetes import client, config
 from kubernetes.client.rest import ApiException
 
 
-
 def cordon_node(cluster_api, node):
     print("Cordoning Node " + node)
     patch_body = {
@@ -27,6 +26,16 @@ def cordon_node(cluster_api, node):
         print("Exception when cordoning %s: %s\n" % (node, error))
 
 
+def get_evictable_pods(cluster_api, node):
+    field_selector = 'spec.nodeName=' + node
+    pods = cluster_api.list_pod_for_all_namespaces(watch=False, field_selector=field_selector)
+    pods_to_evict = []
+
+    for pod in pods.items:
+        if pod.metadata.owner_references[0].kind != 'DaemonSet':
+            pods_to_evict.append(pod)
+    print(pods_to_evict)
+
 
 def lambda_handler(node):
     # Load the config file and init the client
@@ -35,5 +44,8 @@ def lambda_handler(node):
 
     print("Recieved a request to evict node", node)
     cordon_node(cluster_api, node)
+
+    all_evicatable_pods = get_evictable_pods(cluster_api, node)
+    print(all_evicatable_pods)
 
 lambda_handler("k3d-cluster1-agent-0")
