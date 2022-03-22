2722a1ad8ef5e6dd3fd28e741e22c05b8a1fc902
leonardo.wolo <leonardo.wolo@aca-it.be>
2022-03-22 20:04:42 +0100

    python code

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,10 +1,13 @@
 import sys
 import yaml
 import time
+import logging
 
 
 from kubernetes import client, config
 from kubernetes.client.rest import ApiException
+logging.basicConfig(level=logging.DEBUG)
+
 
 
 def cordon_node(cluster_api, node):
@@ -34,7 +37,20 @@ def get_evictable_pods(cluster_api, node):
     for pod in pods.items:
         if pod.metadata.owner_references[0].kind != 'DaemonSet':
             pods_to_evict.append(pod)
-    print(pods_to_evict)
+    return pods_to_evict
+
+
+def evict_pod(cluster_api, pod_name, namespace):
+    print("Evicting", pod_name, "in namespace", namespace, "!")
+    delete_options = client.V1DeleteOptions()
+    delete_options.grace_period_seconds = 750
+    metadata = client.V1ObjectMeta(name=pod_name, namespace=namespace)
+    body = client.V1Eviction(metadata=metadata, api_version="policy/v1", kind="Eviction", delete_options=delete_options)
+
+    try:
+        cluster_api.create_namespaced_pod_eviction(name=pod_name, namespace=namespace, body=body)
+    except ApiException as error:
+        print("Exception when evicting %s: %s\n" % (pod_name, error))
 
 
 def lambda_handler(node):
@@ -46,6 +62,25 @@ def lambda_handler(node):
     cordon_node(cluster_api, node)
 
     all_evicatable_pods = get_evictable_pods(cluster_api, node)
-    print(all_evicatable_pods)
+    for pod in all_evicatable_pods:
+        evict_pod(cluster_api, pod.metadata.name, pod.metadata.namespace)
+    
+    remaining_pods = []
+    endtime = time.time() + 60 * 3
+
+    while time.time() < endtime:
+        remaining_pods = get_evictable_pods(cluster_api, node)
+        if not remaining_pods:
+            print("All pods have been evicted.  Safe to proceed with node termination")
+            break
+        else:
+            print("Waiting for " + str(len(remaining_pods)) + " pods to evict!")
+            time.sleep(5)
+
+    if remaining_pods:
+        print("The following pods did not drain successfully:")
+        for pod in remaining_pods:
+            print(pod.metadata.namespace + "/" + pod.metadata.name)
+        time.sleep(30)
 
 lambda_handler("k3d-cluster1-agent-0")
